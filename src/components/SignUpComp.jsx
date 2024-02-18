36a9a16a443be73cd01fe2fc6e67bdf66cb1c7a2
rahmaAmer1000 <rahmaAmer1000@gmail.com>
2024-02-18 18:08:27 +0200

    updateee

--- src/components/SignUpComp.jsx
+++ src/components/SignUpComp.jsx
@@ -33,7 +33,7 @@ export default function SignUpComp({ router, handleLogin }) {
       );
       console.log("User created successfully!", userCredential.user);
       alert("User created successfully!");
-      router.push("Dashboard/Dashboard");
+      router.push(src\Pages\Dashboard\Dashboard.jsx);
     
     } catch (error) {
       console.error("Login failed:", error.message);
